daa6ee93f33184a15d02464feb3ed69ad5069d4d
mskrasnov <linuxoid85@gmail.com>
2025-01-03 14:34:30 +0300

    Add filename compressor

--- src/downloader.rs
+++ src/downloader.rs
@@ -4,8 +4,14 @@ use anyhow::{Error, Result};
 use colored::Colorize;
 use futures_util::StreamExt;
 use indicatif::{ProgressBar, ProgressStyle};
+use md5::compute;
 use reqwest::Client;
-use std::{cmp::min, fs::File, io::Write, path::Path};
+use std::{
+    cmp::min,
+    fs::{read, File},
+    io::Write,
+    path::Path,
+};
 
 #[tokio::main]
 pub async fn download<U, P>(client: &Client, url: U, path: Option<P>, prefix: P) -> Result<()>
@@ -33,7 +39,7 @@ where
     };
     let prefix = prefix.as_ref();
 
-    let total_size = res.content_length().unwrap_or(666) / 1024;
+    let total_size = res.content_length().unwrap_or(u64::MAX) / 1024;
 
     let pb = ProgressBar::new(total_size);
     pb.set_style(
@@ -41,7 +47,12 @@ where
             .template("{msg} [{bar:20}] {percent}% {elapsed}/{eta} {bytes_per_sec}")?
             .progress_chars("#> "),
     );
-    pb.set_message(format!("Downloading '{}'", &path.dimmed()));
+    let hdr = format!(
+        "{:<width$}",
+        format!("Downloading '{}'", compress_name(&path, 20).dimmed()),
+        width = 45
+    );
+    pb.set_message(hdr);
 
     let mut file = File::create(prefix.join(path))?;
     let mut downloaded: u64 = 0;
@@ -57,4 +68,63 @@ where
     pb.finish();
 
     Ok(())
+}
+
+fn compress_name(name: &str, size_max: usize) -> String {
+    let len = name.len() - 1;
+    if size_max > len {
+        return name.to_string();
+    }
+    let mut unneeded_chars = if len > size_max { 0 } else { size_max - len };
+    if unneeded_chars % 2 == 0 {
+        unneeded_chars /= 2;
+    }
+    unneeded_chars += 1;
+
+    let chars_cnt = len / 2;
+    let chars_first = if chars_cnt > unneeded_chars {
+        chars_cnt - unneeded_chars
+    } else {
+        chars_cnt
+    };
+
+    let mut i = 0;
+    let msg_chars = name.chars().collect::<Vec<_>>();
+    let mut msg_new_chars = Vec::with_capacity(size_max);
+
+    while i < chars_first {
+        msg_new_chars.push(msg_chars[i]);
+        i += 1;
+    }
+
+    let mut j = 1;
+    while j <= 3 {
+        msg_new_chars.push('.');
+        j += 1;
+    }
+
+    i += 4;
+    while i <= len {
+        msg_new_chars.push(msg_chars[i]);
+        i += 1;
+    }
+
+    let mut s = String::new();
+    for ch in msg_new_chars {
+        s.push(ch);
+    }
+
+    if s.contains("....") {
+        s = s.replace("....", "...");
+    }
+
+    s
+}
+
+pub fn check_md5<P: AsRef<Path>>(pth: P, md5: &str) -> Result<bool> {
+    let data = read(&pth)?;
+    let digest = compute(&data);
+    let a = format!("{:?}", digest);
+
+    Ok(&a == md5)
 }
